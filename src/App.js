7b2dcb931da3f00cbba6c70a3f9f409cac9518cf
Bryan Smith <bryan.gsmith33@gmail.com>
2018-09-18 20:29:57 -0600

    start of master branch for students

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
-import ExchangedCurrencyHOC from './Components/HOC'
-import ExchangedCurrencyRP from './Components/RenderProp'
-import Currency from './Components/Currency'
-import Toggle from './Components/Toggle'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toggle render={() => (
-          <Fragment>
-            <h1> Higher Order Component</h1>
-            <ExchangedCurrencyHOC
-              exchangeRate={.76}
-              currency={`Pound: £`} />
-            <ExchangedCurrencyHOC
-              exchangeRate={112.35}
-              currency={`Yen: ¥`} />
-          </Fragment>
-        )}
-          target={`HOC`}
-        />
-        <Toggle render={() => (
-          <Fragment>
-            <h1>Render Prop</h1>
-            <ExchangedCurrencyRP
-              render={(args) => <p>US Dollar: ${args.amount.toFixed(2)} - Krone: kr{(args.amount * 8.15).toFixed(2)}</p>} />
-            <ExchangedCurrencyRP
-              render={(args) => <Currency args={args} currency={`Euro: €`} rate={0.86} />} />
-          </Fragment>
-        )}
-        target={`Render Prop`}
-        />
+
       </div>
     );
   }
